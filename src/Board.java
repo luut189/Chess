883da5274bb48f81b0ccde883834dce789e58966
luut189 <minhtuongluu74@gmail.com>
2022-09-30 00:49:15 -0400

    added legal move, checkmate and random player

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,15 @@
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Board extends JPanel {
     
@@ -23,13 +27,21 @@ public class Board extends JPanel {
     int movedRank = -1, movedFile = -1;
     boolean hasSelected = false;
     
-    int chessBoard[][];
-    int currentTurn, playerToMove;
+    static int chessBoard[][];
+    static int currentTurn, playerToMove;
+
+    static boolean hasCaptured = false;
+    static int capturedPiece = Piece.None;
+
+    boolean endGame = false;
 
     ArrayList<Move> currentAvailableMove = new ArrayList<>();
+    ArrayList<Move> allPossibleMove = new ArrayList<>();
     
     String startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w";
-    String testFen = "rbqnpk2/8/8/8/8/8/8/2KPNQBR b";
+    String testFen = "k7/8/8/8/8/8/8/7K w";
+
+    Random rand = new Random();
 
     Board(int width, int height) {
         chessBoard = new int[8][8];
@@ -40,7 +52,7 @@ public class Board extends JPanel {
         this.setPreferredSize(new Dimension(width, height));
         this.addMouseListener(new mouseAdapter());
 
-        Piece.inputFen(testFen, chessBoard);
+        Piece.inputFen(startFen, chessBoard);
         playerToMove = Piece.getPlayerToMove();
         currentTurn = playerToMove == Piece.White ? 1 : 0;
     }
@@ -54,7 +66,7 @@ public class Board extends JPanel {
     }
 
     // debugging stuff
-    public void getBoard() {
+    public static void getBoard() {
         for(int rank = 0; rank < 8; rank++) {
             for(int file = 0; file < 8; file++) {
                 System.out.print(Piece.getPieceType(chessBoard[rank][file]) + " ");
@@ -118,7 +130,85 @@ public class Board extends JPanel {
         repaint();
     }
 
-    public void getCurrentTurn() {
+    public static int makeMove(int currentPiece, int currentRank, int currentFile, Move move) {
+        if(move.isPromotionMove) {
+            int pieceColor = Piece.getPieceColor(currentPiece);
+            currentPiece = pieceColor | Piece.Q;
+        }
+        hasCaptured = chessBoard[move.rank][move.file] == Piece.None ? false : true;
+        if(hasCaptured) {
+            capturedPiece = chessBoard[move.rank][move.file];
+        }
+        chessBoard[move.rank][move.file] = currentPiece;
+        chessBoard[currentRank][currentFile] = Piece.None;
+        currentTurn++;
+        getCurrentTurn();
+        return playerToMove;
+    }
+
+    public static int unmakeMove(int currentPiece, int currentRank, int currentFile, Move move) {
+        if(move.isPromotionMove) {
+            int pieceColor = Piece.getPieceColor(currentPiece);
+            currentPiece = pieceColor | Piece.P;
+        }
+        chessBoard[currentRank][currentFile] = currentPiece;
+        if(hasCaptured) {
+            chessBoard[move.rank][move.file] = capturedPiece;
+        } else {
+            chessBoard[move.rank][move.file] = Piece.None;
+        }
+        currentTurn--;
+        getCurrentTurn();
+        return playerToMove;
+    }
+
+    public void randomMove() {
+        if(!endGame) {
+            int randRank = rand.nextInt(8);
+            int randFile = rand.nextInt(8);
+            while(chessBoard[randRank][randFile] == Piece.None || Piece.getPieceColor(chessBoard[randRank][randFile]) == Piece.White) {
+                randRank = rand.nextInt(8);
+                randFile = rand.nextInt(8);
+            }
+            getAllPossibleMove();
+            currentAvailableMove = Move.generateMove(chessBoard, chessBoard[randRank][randFile], randRank, randFile, playerToMove);
+            while(allPossibleMove.size() != 0 && currentAvailableMove.size() == 0) {
+                randRank = rand.nextInt(8);
+                randFile = rand.nextInt(8);
+                currentAvailableMove = Move.generateMove(chessBoard, chessBoard[randRank][randFile], randRank, randFile, playerToMove);
+            }
+            if(allPossibleMove.size() == 0) {
+                getEndgame();
+            } else {
+                int randomSelect = rand.nextInt(currentAvailableMove.size());
+                makeMove(chessBoard[randRank][randFile], randRank, randFile, currentAvailableMove.get(randomSelect));
+            }
+        }
+        new Timer(100, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                randomMove();
+                repaint();
+            }
+        }).start();
+    }
+
+    public void getEndgame() {
+        System.out.println((playerToMove == Piece.White ? "black" : "white") + " checkmate");
+        endGame = true;
+        deselectPiece();
+    }
+
+    public void getAllPossibleMove() {
+        allPossibleMove = new ArrayList<>();
+        for(int rank = 0; rank < 8; rank++) {
+            for(int file = 0; file < 8; file++) {
+                allPossibleMove.addAll(0, Move.generateMove(chessBoard, chessBoard[rank][file], rank, file, playerToMove));
+            }
+        }
+    }
+
+    public static void getCurrentTurn() {
         playerToMove = currentTurn % 2 == 0 ? Piece.Black : Piece.White;
     }
 
@@ -132,22 +222,23 @@ public class Board extends JPanel {
                     int currentPiece = chessBoard[selectedRank][selectedFile];
                     int targetPiece = chessBoard[movedRank][movedFile];
                     if(!Piece.isColor(currentPiece, targetPiece)) {
-                        for(int i = 0; i < currentAvailableMove.size(); i++) {
-                            int rank = currentAvailableMove.get(i).rank;
-                            int file = currentAvailableMove.get(i).file;
-                            if(movedRank == rank && movedFile == file) {
-                                if(Piece.getPieceType(currentPiece) == Piece.P) {
+                        for(Move move : currentAvailableMove) {
+                            if(movedRank == move.rank && movedFile == move.file) {
+                                if(move.isPromotionMove) {
                                     int pieceColor = Piece.getPieceColor(currentPiece);
-                                    int rankToPromotion = pieceColor == Piece.White ? 0 : 7;
-                                    if(movedRank == rankToPromotion) {
-                                        currentPiece = pieceColor | Piece.Q;
-                                    }
+                                    currentPiece = pieceColor | Piece.Q;
                                 }
                                 chessBoard[movedRank][movedFile] = currentPiece;
                                 chessBoard[selectedRank][selectedFile] = Piece.None;
                                 currentTurn++;
                                 getCurrentTurn();
-
+                                getAllPossibleMove();
+                                if(allPossibleMove.size() == 0) {
+                                    getEndgame();
+                                    repaint();
+                                } else {
+                                    randomMove();
+                                }
                                 // to print out FEN string for every move
                                 //System.out.println(Piece.outputFen(chessBoard, playerToMove));
                                 break;
@@ -155,12 +246,16 @@ public class Board extends JPanel {
                         }
                     }
                     deselectPiece();
-                } else {
+                } else if(!endGame) {
                     selectedRank = e.getY()/size; // rank
                     selectedFile = e.getX()/size; // file
                     int currentPiece = chessBoard[selectedRank][selectedFile];
                     if(Piece.isTurnToMove(currentPiece, playerToMove)) {
-                        currentAvailableMove = Move.generateMove(chessBoard, currentPiece, selectedRank, selectedFile);
+                        currentAvailableMove = Move.generateMove(chessBoard, currentPiece, selectedRank, selectedFile, playerToMove);
+                        getAllPossibleMove();
+                        if(allPossibleMove.size() != 0 && currentAvailableMove.size() == 0) {
+                            System.out.println("there is no legal move at the current piece");
+                        }
                         hasSelected = true;
                     } else {
                         currentAvailableMove = new ArrayList<>();
--- src/Move.java
+++ src/Move.java
@@ -4,10 +4,12 @@ public class Move {
 
     int rank;
     int file;
+    boolean isPromotionMove;
 
-    Move(int x, int y) {
+    Move(int x, int y, boolean isPromotionMove) {
         this.rank = x;
         this.file = y;
+        this.isPromotionMove = isPromotionMove;
     }
     
     final static int[] possibleSlidingDir = {
@@ -22,6 +24,18 @@ public class Move {
         -7, // up right
         7,  // down left
         9   // down right
+    };
+
+    final static int[][] possibleKingDir = {
+        {-1, 0},
+        {1, 0},
+        {0, -1},
+        {0, 1},
+
+        {-1, -1},
+        {-1, 1},
+        {1, -1},
+        {1, 1}
     };
 
     final static int[][] possibleKnightDir = {
@@ -52,45 +66,120 @@ public class Move {
         {2, 0},  // double push
     };
 
-    public static ArrayList<Move> generateMove(int[][] board, int piece, int currentRank, int currentFile) {
-        ArrayList<Move> availableMove = new ArrayList<>();
-        int pieceType = Piece.getPieceType(piece);
-        if(pieceType != Piece.None) {
-            boolean isSlidingPiece = pieceType == Piece.Q || pieceType == Piece.R || pieceType == Piece.B;
+    public static int[] getKingSquare(int[][] board, int currentPlayer) {
+        int[] kingSquare = new int[2];
 
-            if(isSlidingPiece) {
-                availableMove = generateSlidingMove(board, piece, currentRank, currentFile);
-            } else if(pieceType == Piece.K) {
-                availableMove = generateKingMove(board, piece, currentRank, currentFile);
-            } else if(pieceType == Piece.N) {
-                availableMove = generateKnightMove(board, piece, currentRank, currentFile);
-            } else if(pieceType == Piece.P) {
-                availableMove = generatePawnMove(board, piece, currentRank, currentFile);
+        for(int rank = 0; rank < 8; rank++) {
+            for(int file = 0; file < 8; file++) {
+                if(board[rank][file] == (currentPlayer | Piece.K)) {
+                    kingSquare[0] = rank;
+                    kingSquare[1] = file;
+                    return kingSquare;
+                }
             }
-        } else {
-            availableMove = new ArrayList<>();
+        }
+        return kingSquare;
+    }
+
+    public static boolean checkIllegal(ArrayList<Move> response, int[] kingSquare) {
+        for(Move move : response) {
+            if(move.rank == kingSquare[0] && move.file == kingSquare[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static ArrayList<Move> generateMove(int[][] board, int piece, int currentRank, int currentFile, int currentPlayer) {
+        ArrayList<Move> pseudoLegalMove = generatePseudoLegalMove(board, piece, currentRank, currentFile, currentPlayer);
+        ArrayList<Move> legalMove = new ArrayList<>();
+
+        int[] kingSquare = getKingSquare(board, currentPlayer);
+        for(Move move : pseudoLegalMove) {
+            if(currentRank == kingSquare[0] && currentFile == kingSquare[1]) {
+                Board.makeMove(piece, currentRank, currentFile, move);
+                kingSquare = getKingSquare(board, currentPlayer);
+                Board.unmakeMove(piece, currentRank, currentFile, move);
+                currentPlayer = Board.makeMove(piece, currentRank, currentFile, move);
+                ArrayList<Move> opponentResponse = generateAllPossibleMove(board, currentPlayer);
+                if(!checkIllegal(opponentResponse, kingSquare)) {   
+                    legalMove.add(move);
+                }
+                currentPlayer = Board.unmakeMove(board[move.rank][move.file], currentRank, currentFile, move);
+                kingSquare = getKingSquare(board, currentPlayer);
+            } else {
+                currentPlayer = Board.makeMove(piece, currentRank, currentFile, move);
+                ArrayList<Move> opponentResponse = generateAllPossibleMove(board, currentPlayer);
+                if(!checkIllegal(opponentResponse, kingSquare)) {   
+                    legalMove.add(move);
+                }
+                currentPlayer = Board.unmakeMove(board[move.rank][move.file], currentRank, currentFile, move);
+            }
+        }
+        return legalMove;
+    }
+
+    public static ArrayList<Move> generateAllPossibleMove(int board[][], int currentPlayer) {
+        ArrayList<Move> availableMove = new ArrayList<>();
+
+        for(int rank = 0; rank < 8; rank++) {
+            for(int file = 0; file < 8; file++) {
+                int piece = board[rank][file];
+                if(Piece.isTurnToMove(piece, currentPlayer)) {
+                    int pieceType = Piece.getPieceType(piece);
+                    if(pieceType != Piece.None) {
+                        boolean isSlidingPiece = pieceType == Piece.Q || pieceType == Piece.R || pieceType == Piece.B;
+        
+                        if(isSlidingPiece) {
+                            availableMove.addAll(0, generateSlidingMove(board, piece, rank, file));
+                        } else if(pieceType == Piece.K) {
+                            availableMove.addAll(0, generateKingMove(board, piece, rank, file));
+                        } else if(pieceType == Piece.N) {
+                            availableMove.addAll(0, generateKnightMove(board, piece, rank, file));
+                        } else if(pieceType == Piece.P) {
+                            availableMove.addAll(0, generatePawnMove(board, piece, rank, file));
+                        }
+                    }
+                }
+            }
+        }
+        return availableMove;
+    }
+
+    public static ArrayList<Move> generatePseudoLegalMove(int[][] board, int piece, int currentRank, int currentFile, int currentPlayer) {
+        ArrayList<Move> availableMove = new ArrayList<>();
+        if(Piece.isTurnToMove(piece, currentPlayer)) {
+            int pieceType = Piece.getPieceType(piece);
+            if(pieceType != Piece.None) {
+                boolean isSlidingPiece = pieceType == Piece.Q || pieceType == Piece.R || pieceType == Piece.B;
+
+                if(isSlidingPiece) {
+                    availableMove = generateSlidingMove(board, piece, currentRank, currentFile);
+                } else if(pieceType == Piece.K) {
+                    availableMove = generateKingMove(board, piece, currentRank, currentFile);
+                } else if(pieceType == Piece.N) {
+                    availableMove = generateKnightMove(board, piece, currentRank, currentFile);
+                } else if(pieceType == Piece.P) {
+                    availableMove = generatePawnMove(board, piece, currentRank, currentFile);
+                }
+            }
         }
         return availableMove;
     }
 
     public static ArrayList<Move> generateKingMove(int[][] board, int piece, int currentRank, int currentFile) {
         ArrayList<Move> availableMove = new ArrayList<>();
-
-        int startSquare = currentRank * 8 + currentFile;
-
         for(int i = 0; i < 8; i++) {
-            int targetSquare = startSquare + possibleSlidingDir[i];
-            int targetFile = targetSquare%8;
-            int targetRank = (targetSquare-targetFile)/8;
+            int targetRank = currentRank + possibleKingDir[i][0];
+            int targetFile = currentFile + possibleKingDir[i][1];
 
             if(Piece.isInRange(targetRank, targetFile)) {
                 int pieceOnTarget = board[targetRank][targetFile];
                 if(!Piece.isColor(piece, pieceOnTarget)) {
-                    availableMove.add(new Move(targetRank, targetFile));
-                } else continue;
+                    availableMove.add(new Move(targetRank, targetFile, false));
+                }
             }
         }
-
         return availableMove;
     }
 
@@ -110,7 +199,7 @@ public class Move {
                 int pieceOnTarget = board[targetRank][targetFile];
                 
                 if(Piece.isColor(piece, pieceOnTarget)) break;
-                availableMove.add(new Move(targetRank, targetFile));
+                availableMove.add(new Move(targetRank, targetFile, false));
                 if(Piece.getPieceType(pieceOnTarget) != Piece.None) {
                     if(!Piece.isColor(piece, pieceOnTarget)) {
                         break;
@@ -130,7 +219,7 @@ public class Move {
             if(Piece.isInRange(targetRank, targetFile)) {
                 int pieceOnTarget = board[targetRank][targetFile];
                 if(Piece.isColor(piece, pieceOnTarget)) continue;
-                availableMove.add(new Move(targetRank, targetFile));
+                availableMove.add(new Move(targetRank, targetFile, false));
             } else continue;
         }
         return availableMove;
@@ -143,6 +232,8 @@ public class Move {
         int startRank = pieceColor == Piece.White ? 6 : 1;
         int doublePushIndex = pieceColor == Piece.White ? 2 : 6;
         int startDirIndex = pieceColor == Piece.White ? 0 : 4;
+
+        int promotionRank = pieceColor == Piece.White ? 0 : 7;
         
         for(int i = startDirIndex; i < startDirIndex+2; i++) {
             int targetRank = currentRank + possiblePawnDir[i][0];
@@ -151,7 +242,7 @@ public class Move {
                 int pieceOnTarget = board[targetRank][targetFile];
                 if(Piece.getPieceType(pieceOnTarget) != Piece.None) {
                     if(!Piece.isColor(piece, pieceOnTarget)) {
-                        availableMove.add(new Move(targetRank, targetFile));
+                        availableMove.add(new Move(targetRank, targetFile, targetRank == promotionRank));
                     } else continue;
                 } else continue;
             }
@@ -163,7 +254,7 @@ public class Move {
                 int targetFile = currentFile + possiblePawnDir[i][1];
                 int pieceOnTarget = board[targetRank][targetFile];
                 if(Piece.getPieceType(pieceOnTarget) == Piece.None) {
-                    availableMove.add(new Move(targetRank, targetFile));
+                    availableMove.add(new Move(targetRank, targetFile, false));
                 } else break;
             }
         } else {
@@ -171,7 +262,7 @@ public class Move {
             int targetFile = currentFile + possiblePawnDir[doublePushIndex][1];
             if(Piece.isInRange(targetRank, targetFile)) {
                 int pieceOnTarget = board[targetRank][targetFile];
-                if(Piece.getPieceType(pieceOnTarget) == Piece.None) availableMove.add(new Move(targetRank, targetFile));
+                if(Piece.getPieceType(pieceOnTarget) == Piece.None) availableMove.add(new Move(targetRank, targetFile, targetRank == promotionRank));
             }
         }
 
